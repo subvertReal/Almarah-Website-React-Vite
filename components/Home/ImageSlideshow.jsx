19e5845ce78e7395d741e5443dbc1f80bfd448b0
subvertReal <sherjeelk2004@gmail.com>
2026-07-16 22:02:17 -0400

    Fixed css of arrows

--- components/Home/ImageSlideshow.jsx
+++ components/Home/ImageSlideshow.jsx
@@ -1,15 +1,23 @@
 import Container from 'react-bootstrap/esm/Container'
 import storePage from '../../public/storePage.webp'
 
+// assets
+import lArrow from '../../public/lArrow.png'
+import rArrow from '../../public/rArrow.png'
+
 
 function ImageSlideshow () {
 
     return (
-        <>  <Container className='containerSlideshow'>
-                <button className='slideshowLeft'>testL</button>
-                <button className='slideshowRight'>testR</button>
+        <>  <div className='containerSlideshow'>
+                <button className='slideShowBtn slideshowLeft'>
+                    <img src={lArrow}></img>
+                </button>
+                <button  className='slideShowBtn slideshowRight'>
+                    <img src={rArrow}></img>
+                </button>
                 <img id='storeSlideshow' src={storePage}></img>
-            </Container>
+            </div>
 
 
         </>
