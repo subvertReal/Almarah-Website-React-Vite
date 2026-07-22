ecef2a1831e4c5e4fb7e210f1963dd76914d9ee2
subvertReal <sherjeelk2004@gmail.com>
2026-07-22 10:54:57 -0400

    Created use effect that fetches from api (wip)

--- components/Home/ImageSlideshow.jsx
+++ components/Home/ImageSlideshow.jsx
@@ -1,10 +1,14 @@
 import Container from 'react-bootstrap/esm/Container'
 import storePage1 from '../../public/slideshowAssets/1.webp'
 import storePage2 from '../../public/slideshowAssets/2.webp'
+import { useEffect, useState } from "react";
+
 
 // assets
 import lArrow from '../../public/lArrow.png'
 import rArrow from '../../public/rArrow.png'
+// const api = require('../../src/assets/api.json');
+import jsonData from '../../src/assets/api.json';
 
 
 function slideshowLeft(){
@@ -16,22 +20,44 @@ function slideshowRight(){
     
 }
 
+
+
+
+
+
 function ImageSlideshow () {
 
+    
+    const [slideShowArr, setSlideShowArr] = useState();
+
+
+    useEffect(() => {
+
+        async function getSlideShowData(){
+    
+            console.log('Accessing api link: '+jsonData.apiLink);
+            const response = await fetch(jsonData.apiLink+'/scan');
+
+            return await response.text();
+
+        }
+
+        getSlideShowData();
+
+
+    }, []);
+
     return (
-        <>  <div className='containerSlideshow'>
-                <button onClick={slideshowLeft} className='slideShowBtn slideshowLeft'>
+        <div className='containerSlideshow'>
+                 <button onClick={slideshowLeft} className='slideShowBtn slideshowLeft'>
                     <img src={lArrow}></img>
-                </button>
-                <button onClick={slideshowRight} className='slideShowBtn slideshowRight'>
-                    <img src={rArrow}></img>
-                </button>
-                <img id='storeSlideshow' src={storePage1}></img>
-            </div>
-
-
-        </>
-    )
+                 </button>
+                 <button onClick={slideshowRight} className='slideShowBtn slideshowRight'>
+                     <img src={rArrow}></img>
+                 </button>
+                 <img id='storeSlideshow' src={storePage1}></img>
+             </div>
+    );
 }
 
 export default ImageSlideshow
